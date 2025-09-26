71a275c67d566a30180ef03c4b4ca42741b2e49e
Liroys1 <liroys1@gmail.com>
2025-09-26 14:31:31 +0300

    Add files via upload

---
+++ src/api/entities.js
@@ -0,0 +1,61 @@
+import { base44 } from './base44Client';
+
+
+export const Family = base44.entities.Family;
+
+export const Child = base44.entities.Child;
+
+export const CustodyTemplate = base44.entities.CustodyTemplate;
+
+export const CustodyOverride = base44.entities.CustodyOverride;
+
+export const Event = base44.entities.Event;
+
+export const AuditLog = base44.entities.AuditLog;
+
+export const Homework = base44.entities.Homework;
+
+export const Activity = base44.entities.Activity;
+
+export const Question = base44.entities.Question;
+
+export const Schedule = base44.entities.Schedule;
+
+export const ClassContact = base44.entities.ClassContact;
+
+export const Subject = base44.entities.Subject;
+
+export const Teacher = base44.entities.Teacher;
+
+export const TeacherAssignment = base44.entities.TeacherAssignment;
+
+export const AfterSchoolOverride = base44.entities.AfterSchoolOverride;
+
+export const Chore = base44.entities.Chore;
+
+export const CompletedChore = base44.entities.CompletedChore;
+
+export const Task = base44.entities.Task;
+
+export const CompletedTask = base44.entities.CompletedTask;
+
+export const Member = base44.entities.Member;
+
+export const FamilySummary = base44.entities.FamilySummary;
+
+export const Notification = base44.entities.Notification;
+
+export const NotificationPreference = base44.entities.NotificationPreference;
+
+export const TaskOccurrence = base44.entities.TaskOccurrence;
+
+export const DashboardConfig = base44.entities.DashboardConfig;
+
+export const InviteClaim = base44.entities.InviteClaim;
+
+export const Invite = base44.entities.Invite;
+
+
+
+// auth sdk:
+export const User = base44.auth;
